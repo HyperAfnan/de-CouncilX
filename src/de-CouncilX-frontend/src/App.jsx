4ebf953add2e2f73d98f592b8e58b6ea30ed9b87
HyperAfnan <afnanmalik2712@gmail.com>
2025-07-28 13:50:12 +0530

    added app.jsx

--- src/de-CouncilX-frontend/src/App.jsx
+++ src/de-CouncilX-frontend/src/App.jsx
@@ -1,31 +1,20 @@
-import { useState } from 'react';
-import { de_CouncilX_backend } from 'declarations/de-CouncilX-backend';
+import { useState } from "react";
+import { de_CouncilX_backend } from "declarations/de-CouncilX-backend";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+   /* function handleSubmit(event) {
+     event.preventDefault();
+     const name = event.target.elements.name.value;
+     de_CouncilX_backend.greet(name).then((greeting) => {
+       setGreeting(greeting);
+     });
+     return false;
+   } */
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    de_CouncilX_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
-  return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
-  );
+   return (
+   <div>
+   </div>
+   );
 }
 
 export default App;
